cedd29cdd0b40f4582c5aa8905c6d2ade38a134c
vinay <vinaynotbinay@gmail.com>
2024-02-01 23:03:17 +0530

    add initial files

---
+++ main.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const YoutubeSchema = new mongoose.Schema(
+  {
+    influencer_id:{
+      type: String,
+    },
+    channelId: {
+      type: String,
+      unique: false
+    },
+    access_token:{
+      type: String,
+      unique: false
+    },
+    scope:{
+      type: String,
+      unique: false
+    },
+    token_type:{
+      type: String,
+      unique: false
+    },
+    expiry_date:{
+      type: String,
+      unique: false
+    },
+    viewCount:{
+      type: Number,
+      default: 0,
+      unique: false
+    },
+    likeCount:{
+        type: Number,
+        default: 0,
+        unique: false
+    },
+    subscriberCount: {
+      type: Number,
+      default: 0,
+      unique: false
+    },
+    commentCount: {
+      type: Number,
+      default: 0,
+      unique: false
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("YoutubeSchema", YoutubeSchema);
